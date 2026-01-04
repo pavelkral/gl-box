89abf6e40b8b44819141282645248d10713bbe3a
Pavel Kral <frostxx@tiscali.cz>
2026-01-04 21:18:11 +0100

    Comment out PerformRaycast function in Raycast.h
    The PerformRaycast function has been fully commented out, possibly for refactoring, deprecation, or to temporarily disable its usage. No other logic or interface changes were made.

--- src/glbox/physics/Raycast.h
+++ src/glbox/physics/Raycast.h
@@ -360,59 +360,59 @@ private:
         }
     }
 };
-bool PerformRaycast(const Ray& ray,
-                    const Octree& sceneOctree,
-                    const std::map<StaticMesh*, glm::mat4>& modelMatrices,
-                    RaycastHit& outHit)
-{
-    outHit.hit = false;
-    outHit.distance = FLT_MAX;
-    outHit.object = nullptr;
+// bool PerformRaycast(const Ray& ray,
+//                     const Octree& sceneOctree,
+//                     const std::map<StaticMesh*, glm::mat4>& modelMatrices,
+//                     RaycastHit& outHit)
+// {
+//     outHit.hit = false;
+//     outHit.distance = FLT_MAX;
+//     outHit.object = nullptr;
 
-    // 1. Získáme ze stromu jen relevantní objekty
-    std::vector<StaticMesh*> potentialHits;
-    sceneOctree.Query(ray, potentialHits);
+//     // 1. Získáme ze stromu jen relevantní objekty
+//     std::vector<StaticMesh*> potentialHits;
+//     sceneOctree.Query(ray, potentialHits);
 
-    if (potentialHits.empty()) {
-        return false;
-    }
+//     if (potentialHits.empty()) {
+//         return false;
+//     }
 
-    // 2. Teď provedeme test "brute-force", ALE UŽ JEN NA TĚCH PÁR KANDIDÁTECH
-    for (StaticMesh* mesh : potentialHits) {
+//     // 2. Teď provedeme test "brute-force", ALE UŽ JEN NA TĚCH PÁR KANDIDÁTECH
+//     for (StaticMesh* mesh : potentialHits) {
 
-        // Bezpečné nalezení matice v mapě
-        auto it = modelMatrices.find(mesh);
-        if (it == modelMatrices.end()) {
-            // Tento mesh nemá z nějakého důvodu matici v mapě, přeskočíme ho
-            std::cerr << "err: Raycast not find matrix for mesh!" << std::endl;
-            continue;
-        }
+//         // Bezpečné nalezení matice v mapě
+//         auto it = modelMatrices.find(mesh);
+//         if (it == modelMatrices.end()) {
+//             // Tento mesh nemá z nějakého důvodu matici v mapě, přeskočíme ho
+//             std::cerr << "err: Raycast not find matrix for mesh!" << std::endl;
+//             continue;
+//         }
 
-        // Použijeme matici z parametru
-        const glm::mat4& modelMatrix = it->second;
+//         // Použijeme matici z parametru
+//         const glm::mat4& modelMatrix = it->second;
 
-        // Znovu spočítáme world AABB pro přesný test
-        // (Alternativa: Octree by mohl vracet i AABB, se kterým byl objekt vložen)
-        BoxCollider worldAABB = mesh->localAABB.GetTransformed(modelMatrix);
+//         // Znovu spočítáme world AABB pro přesný test
+//         // (Alternativa: Octree by mohl vracet i AABB, se kterým byl objekt vložen)
+//         BoxCollider worldAABB = mesh->localAABB.GetTransformed(modelMatrix);
 
-        float t; // Vzdálenost zásahu
-        if (worldAABB.Intersects(ray, t)) {
+//         float t; // Vzdálenost zásahu
+//         if (worldAABB.Intersects(ray, t)) {
 
-            // TODO: Zde by měl být přesný test proti trojúhelníkům.
-            // Prozatím bereme zásah AABB jako finální zásah.
+//             // TODO: Zde by měl být přesný test proti trojúhelníkům.
+//             // Prozatím bereme zásah AABB jako finální zásah.
 
-            // 3. Je tento zásah blíž než ten předchozí?
-            if (t < outHit.distance) {
-                outHit.hit = true;
-                outHit.distance = t;
-                outHit.point = ray.origin + ray.direction * t;
-                outHit.object = mesh;
-            }
-        }
-    }
+//             // 3. Je tento zásah blíž než ten předchozí?
+//             if (t < outHit.distance) {
+//                 outHit.hit = true;
+//                 outHit.distance = t;
+//                 outHit.point = ray.origin + ray.direction * t;
+//                 outHit.object = mesh;
+//             }
+//         }
+//     }
 
-    return outHit.hit;
-}
+//     return outHit.hit;
+// }
 
 
 #endif // RAYCAST_H
